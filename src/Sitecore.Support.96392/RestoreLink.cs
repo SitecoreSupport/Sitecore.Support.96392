2cc31e4dcef61d10527716b5ba5ab16b48491529
DK\hly <hly@sitecore.net>
2018-08-29 09:34:28 -0400

    patch for bug 96392. This commit implement a custom hook to register a handler to item restore complete event. The handler recover the link data of recovered items.

---
+++ src/Sitecore.Support.96392/RestoreLink.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Events.Hooks;
+using Sitecore.Eventing;
+using Sitecore.Pipelines;
+using Sitecore.Services;
+using Sitecore.Data.Archiving;
+using Sitecore.Diagnostics;
+using Sitecore.Data.Items;
+using System;
+
+namespace Sitecore.Support.RestoreLink
+{
+   
+    public class RestoreLinkDatabaseHook : IHook
+    {
+       
+
+       
+        public void Initialize()
+        {
+            EventManager.Subscribe<RestoreItemCompletedEvent>(delegate(RestoreItemCompletedEvent e, EventContext c)
+            {
+                Sitecore.Data.ID itemId = new Sitecore.Data.ID(e.ItemId);
+                Sitecore.Caching.CacheManager.ClearAllCaches();
+                Item item = Sitecore.Configuration.Factory.GetDatabase("master").GetItem(itemId);
+                Assert.IsNotNull(item, "the item doesn't exist!");
+                Sitecore.Links.LinkDatabase linkDatabase = Sitecore.Globals.LinkDatabase;          
+                if (linkDatabase != null)
+                {
+                    linkDatabase.UpdateItemVersionReferences(item);
+                }
+                else
+                {
+                    Log.Info("Linkdatabase doesn't exist!", this);
+                }
+            });
+        }
+
+    }
+}
